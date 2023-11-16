17728ab29ce19de26519d6fbc7573d36c6668a50
Mateo Serrato Ascencio <192398-9@ti.leon.uia.mx>
2023-11-16 09:37:30 -0600

    Primer Commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup (name =  "paquete_ibero_al",
+       version = "0.1",
+       py_modules=["ibero_al"]
+       )
+    
---
+++ test_al.py
@@ -0,0 +1,9 @@
+#TEST
+
+
+import import ibero_al as AL
+det2=det2x2() 
+det3=det3x3()
+magn=magnitud()
+direccion =direccion()
+Suma=suma2v()
