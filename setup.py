2c2d169df30411aaf2cab9a1ab60fd1ef7ce4b62
Mauricio-trejo04 <192583-4@iberoleon.edu.mx>
2023-11-29 18:10:46 -0600

    cambios

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
-setup (name =  "paquete_ibero_al",
-       version = "0.1",
-       py_modules=["ibero_al"]
-       install_requires=[
+setup(
+    name="paquete_ibero_al",
+    version="0.1",
+    py_modules=["ibero_al"],
+    install_requires=[
         'numpy',
         'scipy',
         'matplotlib'
-        ]
-       )
-    
+    ]
+)
+
